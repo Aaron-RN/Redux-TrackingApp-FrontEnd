90b2a5a986658a36cab6ef20dd066250992c8914
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-06-29 14:11:34 -0400

    Added calories by day

--- src/components/functional/foodList.js
+++ src/components/functional/foodList.js
@@ -21,6 +21,27 @@ const FoodList = ({
       <Food key={meal.id + meal.name} foodsList={foods} meal={meal} removeFood={removeFood} />
     );
   });
+  const caloriesByDay = dayChosen => {
+    let CaloriesForMeals = 0;
+    foods.forEach(meal => {
+      const {
+        /* eslint-disable camelcase */
+        date_consumed, servings_consumed, carbs, fats, proteins,
+      } = meal;
+      const date = new Date(date_consumed);
+      const day = date.toLocaleDateString('en-US', { weekday: 'long' });
+
+      const carbCalories = carbs * 4;
+      const fatCalories = fats * 9;
+      const proteinCalories = proteins * 4;
+      const totalCalories = (carbCalories + fatCalories + proteinCalories) * servings_consumed;
+
+      if (day === dayChosen) {
+        CaloriesForMeals += totalCalories;
+      }
+    });
+    return CaloriesForMeals;
+  };
   const { isLoading } = status;
   const renderMain = isLoading
     ? (
@@ -33,30 +54,58 @@ const FoodList = ({
       <div ref={mealsContainer} className="foods-section">
         <div>
           <h2>Sunday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Sunday')}
+          </div>
           {mealByDay('Sunday')}
         </div>
         <div>
           <h2>Saturday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Saturday')}
+          </div>
           {mealByDay('Saturday')}
         </div>
         <div>
           <h2>Friday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Friday')}
+          </div>
           {mealByDay('Friday')}
         </div>
         <div>
           <h2>Thursday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Thursday')}
+          </div>
           {mealByDay('Thursday')}
         </div>
         <div>
           <h2>Wednesday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Wednesday')}
+          </div>
           {mealByDay('Wednesday')}
         </div>
         <div>
           <h2>Tuesday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Tuesday')}
+          </div>
           {mealByDay('Tuesday')}
         </div>
         <div>
           <h2>Monday</h2>
+          <div>
+            <span>Total Calories: </span>
+            {caloriesByDay('Monday')}
+          </div>
           {mealByDay('Monday')}
         </div>
       </div>
